5972dc553a17c3bfb92d64eedf76cf14ca9bc4bf
EmbersAlice <killian.alice.p@gmail.com>
2024-10-15 14:34:41 -0400

    GUI and reusability added

--- Encounter_Generator.py
+++ Encounter_Generator.py
@@ -1,74 +1,213 @@
 import json
-import tkinter
+from tkinter import *
+from tkinter import messagebox
 
-# Error catching for the user inputs
-def get_pos_num(st):
-	isNotValid = True
-	while isNotValid:
-		try:
-			num = int(input(st))
-			if num < 1:
-				print("Enter a positive integer.")
-				num = int(input(st))
-			else:
-				isNotValid = False
-		except:
-			print("Enter an integer.")
-	return num
+# Primary Window
+class Application:
 
-#	Initialize the number of players and their level, and
-#	how many mosters if they would like
-player_count = get_pos_num("How many players do you have?")
+	def __init__(self, root, title, geometry):
+		self.root = root
 
-avg_level = get_pos_num("What is the aproximate average level of your party?")
+		# Full window resizing, add more columns/rows 
+		# as needed for frame orginization
+		# Currently: 2 Columns, 1 Rows
+		self.root.columnconfigure(0, weight = 1)
+		self.root.columnconfigure(1, weight = 2)
+		self.root.rowconfigure(0, weight = 1)
 
-monster_num = get_pos_num("How many monsters would you prefer, if a specific amount?")
+		# Title Construction
+		self.root.title(title)
+
+		# Geometry Construction
+		self.root.geometry(geometry)   # Input size (XXXXxYYYY)
+
+		# Widget Construction
+		self.create_widgets()
+
+		# Call JSON handling method
+		self.format_json()
+
+		# Create a list to add results from the balancing math
+		# and the monster_list
+		self.root.result_list = []
+
+		# Bind left click within result window to
+		# set the clicked name to be the chosen result.
+		# and display the chosen result in the right frame's
+		# text window.
+		self.root.monsterResults.bind("<ButtonRelease-1>", self.select_line, add = "+")
+		self.root.monsterResults.bind("<ButtonRelease-1>", self.display_results, add = "+")
+
+		# Mainloop Construction
+		self.root.mainloop()
+		
+	# Widget Construction Function
+	def create_widgets(self):
+
+		# Overall Frames
+		self.root.leftFrame = Frame(self.root, bg = 'red')
+		self.root.rightFrame = Frame(self.root, bg = 'black')
+		
+		# Left Frame Widgets
+		self.root.numOfPlayersLbl = Label(self.root.leftFrame, text = "Hows many players do you have?")
+		self.root.playersEntry = Entry(self.root.leftFrame, width= 50)
+		self.root.playerLevelLbl = Label(self.root.leftFrame, text = "What is the average level of your players?")
+		self.root.playerLevelEntry = Entry(self.root.leftFrame, width = 50)
+		self.root.numOfMonstersLbl = Label(self.root.leftFrame, text = "How many monsters would like for the encounter?")
+		self.root.numOfMonstersEntry = Entry(self.root.leftFrame, width = 50)
+		self.root.crButton = Button(self.root.leftFrame, text = "Click to calculate \nChallenge Rating", command = self.on_click)
+		self.root.monsterResultsLbl = Label(self.root.leftFrame, text = "List of Monsters with appropriate challenge ratings | Click chosen monster to display it's statistics")
+		self.root.monsterResults = Text(self.root.leftFrame, width=60, height = 20)
+
+		# Right Frame Widgets
+		self.root.chosenMonsterLbl = Label(self.root.rightFrame, text = "Chosen Monsters Statistics:")
+		self.root.chosenMonsterInfo = Text(self.root.rightFrame, width=60, height = 20)
+
+		# Orginization
+
+		# Frames
+		self.root.leftFrame.grid(column = 0, row = 0, sticky = "NSEW")
+		self.root.rightFrame.grid(column = 1, row = 0, sticky = "NSEW")
+
+		# Left Frame Widgets
+		self.root.numOfPlayersLbl.grid(column = 0, row = 0)
+		self.root.playersEntry.grid(column = 0, row = 1)
+		self.root.playerLevelLbl.grid(column = 0, row  = 2)
+		self.root.playerLevelEntry.grid(column = 0, row = 3)
+		self.root.numOfMonstersLbl.grid(column = 0, row = 4)
+		self.root.numOfMonstersEntry.grid(column = 0, row = 5)
+		self.root.crButton.grid(column = 0, row = 6, sticky = "E")
+		self.root.monsterResultsLbl.grid(column = 0, row = 7)
+		self.root.monsterResults.grid(column = 0, row = 8)
+
+		# Right Frame Widgets
+		self.root.chosenMonsterLbl.grid(column = 0, row = 0, sticky = "S", pady = 5)
+		self.root.chosenMonsterInfo.grid(column = 0, row = 1, sticky = "N", pady  = 5)
 
 
-#   Using the above inputs, generate a number to search for monsters
-#	with appropriate Challenge Rating for the party in the dictionary
-if player_count > 5:
-	CR_Balance = avg_level + (player_count - 5)
-elif player_count >= 3 and player_count <= 5:
-	CR_Balance = avg_level
-elif player_count < 3:
-	CR_Balance = avg_level - 1
-elif player_count < 3 and avg_level == 1:
-	CR_Balance = 1
+		# Variables for dynamic resizing
+		# of frames/allows widgets to center
+		rowNum = 0
+		columnNum = 0
+		
+		# Left Frame resizing
+		for rowNum in range(self.root.leftFrame.grid_size()[1]):
+			self.root.leftFrame.rowconfigure(rowNum, weight = 1)
+			rowNum += 1
 
-#	Open the file with a error catch
-try:
-	monsters = open("5e_Monsters.json", "r")
-except:
-	print("Error reading '5e_Monsters' file")
+		for columnNum in range(self.root.leftFrame.grid_size()[0]):
+			self.root.leftFrame.columnconfigure(columnNum, weight = 1)
+			columnNum += 1
 
-#	Reading the file
-monster_list = json.loads(monsters.read())
+		# Right Frame resizing
+		for rowNum in range(self.root.rightFrame.grid_size()[1]):
+			 self.root.rightFrame.rowconfigure(rowNum, weight = 1)
+			 rowNum += 1
 
-#	Create a list to add results from the balancing math
-#	and the monster_list
+		for columnNum in range(self.root.rightFrame.grid_size()[0]):
+			self.root.rightFrame.columnconfigure(columnNum, weight = 1)
+			columnNum += 1
 
-result_list = []
+	# Iterate over the list of monsters setting the challenge
+	# rating against the CR_Balance, adding the results to result_list
+	def determine_results(self):
+		# Refresh result list
+		self.root.result_list.clear()
 
-#	Iterate over the list of monsters setting the challenge
-# 	rating against the CR_Balance from earlier, adding the
-#	results to result_list
+		for self.root.aMonster in self.root.monster_list:
+			if self.root.numOfMonsters > 1:
+				if self.root.aMonster["challenge_rating"] == (self.root.CR_Balance//self.root.numOfMonsters):
+					self.root.result_list.append(self.root.aMonster)
+			if self.root.aMonster["challenge_rating"] == self.root.CR_Balance:
+				self.root.result_list.append(self.root.aMonster)
 
-for amonster in monster_list:
-	if monster_num > 1:
-		if amonster["challenge_rating"] == (CR_Balance//monster_num):
-			result_list.append(amonster)
-	if amonster["challenge_rating"] == CR_Balance:
-		result_list.append(amonster)
+		for self.root.result in self.root.result_list:
+			self.root.monsterResults.insert(END, self.root.result["name"] +  "\n")
 
-#   Print the names and challenge ratings of the monsters
-#   that have the right CR_Balance
-for result in result_list:
-	print(result["name"],result["challenge_rating"])
 
-#   Ask for a specific monster and print that monster's details
-info_request = input("Enter the name of the monster you would like information on:")
-for result in result_list:
-	if info_request.lower() == result["name"].lower():
-		the_chosen_one = result
-print(json.dumps(the_chosen_one, indent = 2))
+	# Function for opening and formatting
+	# the json file of monster statistics
+	def format_json(self):
+			# Open the file with a error catch
+			try:
+				self.root.monsters = open("5e_Monsters.json", "r")
+			except:
+				messagebox.showerror('Python Error', 'Error reading file')
+
+			#Reading the file
+			self.root.monster_list = json.loads(self.root.monsters.read())
+			# Test Display of all monsters if needed:
+			# self.root.formatted_json = json.dumps(self.root.monster_list, indent = 2)
+
+	
+	# Error catch for user input
+	def get_pos_num(self, st):
+		isNotValid = True
+		while isNotValid:
+			try:
+				num = int(st)
+				if num < 1:
+					messagebox.showerror('Python Error', 'Please enter a positive integer for all fields.')
+					return
+				else:
+					isNotValid = False
+			except:
+				messagebox.showerror('Python Error', 'Please enter an integer for all fields.')
+				return
+		return num	
+
+	# Function to determine the appropriate
+	# challenge rating.
+	def CR_Calculation(self):
+		if self.root.numOfPlayers > 5:
+			self.root.CR_Balance = self.root.playerLevel + (self.root.numOfPlayers - 5)
+		elif self.root.numOfPlayers >= 3 and self.root.numOfPlayers <= 5:
+			self.root.CR_Balance = self.root.playerLevel
+		elif self.root.numOfPlayers < 3:
+			self.root.CR_Balance = self.root.playerLevel - 1
+		elif self.root.numOfPlayers < 3 and self.root.playerLevel == 1:
+			self.root.CR_Balance = 1
+		return self.root.CR_Balance
+	
+	# Get user inputs from entry fields
+	# upon button click and verify with 
+	# error catch
+	def on_click(self):
+		self.root.monsterResults.delete(1.0, END)
+		self.root.numOfPlayers = self.root.playersEntry.get()
+		self.root.playerLevel = self.root.playerLevelEntry.get()
+		self.root.numOfMonsters = self.root.numOfMonstersEntry.get()
+		self.root.numOfPlayers = self.get_pos_num(self.root.numOfPlayers)
+		self.root.playerLevel = self.get_pos_num(self.root.playerLevel)
+		self.root.numOfMonsters = self.get_pos_num(self.root.numOfMonsters)
+		self.root.CR_Balance = self.CR_Calculation()
+		self.determine_results()
+	
+	# Function to make left click to set Chosen
+	# result within result window. 
+	def select_line(self, event):
+		self.root.chosenResult = self.root.monsterResults.get('insert linestart', 'insert lineend')
+		
+	# Function to make left click also Display
+	# chosen results in final result window in 
+	# the right frame.
+	def display_results(self,event):
+		self.root.chosenMonsterInfo.delete(1.0, END)
+		for self.root.result in self.root.result_list:
+			if self.root.chosenResult.lower() == self.root.result["name"].lower():
+				self.root.finalResult = json.dumps(self.root.result, indent = 2)
+		self.root.chosenMonsterInfo.insert(END, self.root.finalResult)
+				
+
+# Main Function
+def main():
+	root = Tk()
+	root.columnconfigure(0, weight = 1)
+	root.rowconfigure(0, weight = 1)
+	testApp = Application(root, "5th Edition Encounter Generator", "1024x576")
+	root.mainloop()
+	return None
+
+# Run Main Function
+main()
+
